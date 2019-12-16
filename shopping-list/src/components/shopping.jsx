41b2428c3ad1ef144c8160b27bcb031171e6e340
Mario90 <mgpinan1990@gmail.com>
2019-12-16 18:05:40 +0000

    Added button to increment

--- shopping-list/src/components/shopping.jsx
+++ shopping-list/src/components/shopping.jsx
@@ -1,10 +1,16 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 
 class Shopping extends Component {
-    // state = {  }
-    render() { 
-        return <h1>Hello World</h1>;
-    }
+  // state = {  }
+  render() {
+    React.createElement("div");
+    return (
+      <div>
+        <h1>Hello World</h1>
+        <button>Increment</button>
+      </div>
+    );
+  }
 }
- 
+
 export default Shopping;
