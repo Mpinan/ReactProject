eafe9a486df48578a9fdb5e28facc4c2b1e75e23
Mario90 <mgpinan1990@gmail.com>
2019-12-28 10:04:27 +0000

    Added length of array to navbar, extracted logic to app.js

--- shopping-list/src/App.js
+++ shopping-list/src/App.js
@@ -4,12 +4,49 @@ import Shoppings from "./components/shoppings";
 import "./App.css";
 
 class App extends Component {
+  state = {
+    items: [
+      { id: 1, value: 4 },
+      { id: 2, value: 0 },
+      { id: 3, value: 0 },
+      { id: 4, value: 0 },
+      { id: 5, value: 0 }
+    ]
+  };
+
+  handleIncrement = item => {
+    console.log(item);
+    const items = [...this.state.items];
+    const index = items.indexOf(item);
+    items[index] = { ...item };
+    items[index].value++;
+    this.setState({ items });
+  };
+
+  handleDelete = itemId => {
+    const items = this.state.items.filter(i => i.id !== itemId);
+    this.setState({ items });
+  };
+
+  handleReset = () => {
+    const items = this.state.items.map(i => {
+      i.value = 0;
+      return i;
+    });
+    this.setState({ items });
+  };
+
   render() {
     return (
       <React.Fragment>
-        <NavBar />
+        <NavBar totalItems={this.state.items.filter(i => i.value > 0).length} />
         <main className="container">
-          <Shoppings />
+          <Shoppings
+            items={this.state.items}
+            onReset={this.handleReset}
+            onDelete={this.handleDelete}
+            onIncrement={this.handleIncrement}
+          />
         </main>
       </React.Fragment>
     );
--- shopping-list/src/components/shoppings.jsx
+++ shopping-list/src/components/shoppings.jsx
@@ -2,52 +2,20 @@ import React, { Component } from "react";
 import Shopping from "./shopping";
 
 class Shoppings extends Component {
-  state = {
-    items: [
-      { id: 1, value: 4 },
-      { id: 2, value: 0 },
-      { id: 3, value: 0 },
-      { id: 4, value: 0 },
-      { id: 5, value: 0 }
-    ]
-  };
-
-  handleIncrement = item => {
-    console.log(item);
-    const items = [...this.state.items];
-    const index = items.indexOf(item);
-    items[index] = { ...item };
-    items[index].value++;
-    this.setState({ items });
-  };
-
-  handleDelete = itemId => {
-    const items = this.state.items.filter(i => i.id !== itemId);
-    this.setState({ items });
-  };
-
-  handleReset = () => {
-    const items = this.state.items.map(i => {
-      i.value = 0;
-      return i;
-    });
-    this.setState({ items });
-  };
-
   render() {
     return (
       <div>
         <button
-          onClick={this.handleReset}
+          onClick={this.props.onReset}
           className="btn btn-primary btn-smalls m-2"
         >
           Reset
         </button>
-        {this.state.items.map(item => (
+        {this.props.items.map(item => (
           <Shopping
             key={item.id}
-            onDelete={this.handleDelete}
-            onIncrement={this.handleIncrement}
+            onDelete={this.props.onDelete}
+            onIncrement={this.props.onIncrement}
             item={item}
           />
         ))}
