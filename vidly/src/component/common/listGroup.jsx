1f7700e120df2eac5ace6991fc3ff5cecd04b159
Mario90 <mgpinan1990@gmail.com>
2020-01-06 15:08:10 +0000

    Added handle event for the genre

---
+++ vidly/src/component/common/listGroup.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+
+const List = () => {
+  return (
+    <ul className="list-group">
+      <li className="list-group-item">Action</li>
+      <li className="list-group-item">Thriller</li>
+      <li className="list-group-item">Comedy</li>
+    </ul>
+  );
+};
+
+export default List;
