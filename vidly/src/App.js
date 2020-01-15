a2a83a79a6fd61a698103dff1068367d83f1d7d4
Mario90 <mgpinan1990@gmail.com>
2020-01-15 18:11:29 +0000

    Added navBar

--- vidly/src/App.js
+++ vidly/src/App.js
@@ -4,21 +4,25 @@ import Movies from "./component/movies";
 import Customers from "./component/customers";
 import Rentals from "./component/rentals";
 import NotFound from "./component/notFound";
+import NavBar from "./component/common/navBar";
 import "./App.css";
 
 class App extends Component {
   render() {
     return (
-      <main className="container">
-        <Switch>
-          <Route path="/movies" component={Movies}></Route>
-          <Route path="/customers" component={Customers}></Route>
-          <Route path="/rentals" component={Rentals}></Route>
-          <Route path="/not-found" component={NotFound}></Route>
-          <Redirect from="/" exact to="/movies" />
-          <Redirect to="/not-found" />
-        </Switch>
-      </main>
+      <React.Fragment>
+        <NavBar />
+        <main className="container">
+          <Switch>
+            <Route path="/movies" component={Movies}></Route>
+            <Route path="/customers" component={Customers}></Route>
+            <Route path="/rentals" component={Rentals}></Route>
+            <Route path="/not-found" component={NotFound}></Route>
+            <Redirect from="/" exact to="/movies" />
+            <Redirect to="/not-found" />
+          </Switch>
+        </main>
+      </React.Fragment>
     );
   }
 }
--- vidly/src/component/common/navBar.jsx
+++ vidly/src/component/common/navBar.jsx
@@ -1,20 +1,38 @@
-// import React from "react";
-// import { Link } from "react-router-dom";
+import React from "react";
+import { Link, NavLink } from "react-router-dom";
 
-// const NavBar = () => {
-//   return (
-//     <ul>
-//       <li>
-//         <Link to="/movies">Movies</Link>
-//       </li>
-//       <li>
-//         <Link to="/customers">Customers</Link>
-//       </li>
-//       <li>
-//         <Link to="/rentals">Rentals</Link>
-//       </li>
-//     </ul>
-//   );
-// };
+const NavBar = () => {
+  return (
+    <nav className="navbar navbar-expand-lg navbar-light bg-light">
+      <Link className="navbar-brand" to="/">
+        Vidly
+      </Link>
+      <button
+        className="navbar-toggler"
+        type="button"
+        data-toggle="collapse"
+        data-target="#navbarNav"
+        aria-controls="navbarNav"
+        aria-expanded="false"
+        aria-label="Toggle navigation"
+      >
+        <span className="navbar-toggler-icon"></span>
+      </button>
+      <div className="collapse navbar-collapse" id="navbarNav">
+        <div className="navbar-nav">
+          <NavLink className="nav-item nav-link" to="/movies">
+            Movies
+          </NavLink>
+          <NavLink className="nav-item nav-link" to="/customers">
+            Customers
+          </NavLink>
+          <NavLink className="nav-item nav-link" to="/rentals">
+            Rentals
+          </NavLink>
+        </div>
+      </div>
+    </nav>
+  );
+};
 
-// export default NavBar;
+export default NavBar;
