ade50c065ce979057f167c251be8144543c1de84
Mario90 <mgpinan1990@gmail.com>
2020-01-16 21:29:09 +0000

    Handling inputs for password

--- vidly/src/component/loginForm.jsx
+++ vidly/src/component/loginForm.jsx
@@ -9,12 +9,14 @@ class Login extends Component {
     console.log("Submitted");
   };
 
-  handleChange = e => {
+  handleChange = ({ currentTarget: input }) => {
     const account = { ...this.state.account };
-    account.username = e.currentTarget.value;
+    account[input.name] = input.value;
+
     this.setState({ account });
   };
   render() {
+    const { account } = this.state;
     return (
       <div>
         <h1>Login</h1>
@@ -22,10 +24,12 @@ class Login extends Component {
           <div className="form-group">
             <label htmlFor="username">Username</label>
             <input
-              value={this.state.account.username}
+              value={account.username}
               onChange={this.handleChange}
               autoFocus
               ref={this.username}
+              id="username"
+              name="username"
               type="text"
               className="form-control"
               placeholder="Username"
@@ -37,9 +41,12 @@ class Login extends Component {
           <div className="form-group">
             <label htmlFor="password">Password</label>
             <input
+              value={account.password}
+              onChange={this.handleChange}
+              name="password"
+              id="password"
               type="password"
               className="form-control"
-              id="password"
               placeholder="Password"
             />
           </div>
