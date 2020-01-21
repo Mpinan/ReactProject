1963231c5668c0c43a70e40168fe05cae133ffd7
Mario90 <mgpinan1990@gmail.com>
2020-01-21 16:56:47 +0000

    Added validations property

--- vidly/src/component/loginForm.jsx
+++ vidly/src/component/loginForm.jsx
@@ -12,9 +12,9 @@ class Login extends Component {
 
     const { account } = this.state;
     if (account.username.trim() === "")
-      errors.username = "Username is required";
+      errors.username = "Username is required.";
     if (account.password.trim() === "")
-      errors.password = "Password is required";
+      errors.password = "Password is required.";
     return Object.keys(errors).length === 0 ? null : errors;
   };
 
@@ -29,11 +29,28 @@ class Login extends Component {
     console.log("submitted");
   };
 
+  validateProperty = ({ name, value }) => {
+    if (name === "username") {
+      if (value.trim() === "") return "Username is required.";
+      //...
+    }
+    if (name === "password") {
+      if (value.trim() === "") return "Password is required.";
+      //...
+    }
+    console.log(name, value);
+  };
+
   handleChange = ({ currentTarget: input }) => {
+    const errors = { ...this.state.errors };
+    const errorMessage = this.validateProperty(input);
+    if (errorMessage) errors[input.name] = errorMessage;
+    else delete errors[input.name];
+
     const account = { ...this.state.account };
     account[input.name] = input.value;
 
-    this.setState({ account });
+    this.setState({ account, errors });
   };
   render() {
     const { account, errors } = this.state;
