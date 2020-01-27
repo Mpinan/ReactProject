f8f668ee23703ae0ee53ce422335a056c7e15146
Mario90 <mgpinan1990@gmail.com>
2020-01-27 12:24:59 +0000

    ADded new common form, extracted methods in form component

--- vidly/src/component/loginForm.jsx
+++ vidly/src/component/loginForm.jsx
@@ -1,10 +1,11 @@
 import React, { Component } from "react";
 import Input from "./common/input";
 import Joi from "joi-browser";
+import Form from "./common/form";
 
-class Login extends Component {
+class Login extends Form {
   state = {
-    account: { username: "", password: "" },
+    data: { username: "", password: "" },
     errors: {}
   };
 
@@ -17,51 +18,17 @@ class Login extends Component {
       .label("Password")
   };
 
-  validate = () => {
-    const options = { abortEarly: false };
-    const { error } = Joi.validate(this.state.account, this.schema, options);
-    if (!error) return null;
-    const errors = {};
-    for (let item of error.details) errors[item.path[0]] = item.message;
-    return errors;
-  };
-
-  handleLogin = e => {
-    e.preventDefault();
-
-    const errors = this.validate();
-
-    this.setState({ errors: errors || {} });
-    if (errors) return;
-
+  doSubmit = () => {
+    // call serveer
     console.log("submitted");
   };
 
-  validateProperty = ({ name, value }) => {
-    const obj = { [name]: value };
-    const schema = { [name]: this.schema[name] };
-    const { error } = Joi.validate(obj, schema);
-    return error ? error.details[0].message : null;
-    // console.log(name, value);
-  };
-
-  handleChange = ({ currentTarget: input }) => {
-    const errors = { ...this.state.errors };
-    const errorMessage = this.validateProperty(input);
-    if (errorMessage) errors[input.name] = errorMessage;
-    else delete errors[input.name];
-
-    const account = { ...this.state.account };
-    account[input.name] = input.value;
-
-    this.setState({ account, errors });
-  };
   render() {
     const { account, errors } = this.state;
     return (
       <div>
         <h1>Login</h1>
-        <form onSubmit={this.handleLogin}>
+        <form onSubmit={this.handleSubmit}>
           <Input
             name="username"
             value={account.username}
