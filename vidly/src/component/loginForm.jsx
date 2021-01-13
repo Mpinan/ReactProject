0f9560f76980718201f76129cc9005f967a0b164
jaitone <jaitoone@gmail.com>
2021-01-13 11:49:08 +0000

    modified

--- vidly/src/component/loginForm.jsx
+++ vidly/src/component/loginForm.jsx
@@ -1,34 +1,46 @@
 import React, { Component } from "react";
 import Input from "./common/input";
-import Joi from "joi-browser";
-import Form from "./common/form";
 
-class Login extends Form {
+class Login extends Component {
   state = {
-    data: { username: "", password: "" },
+    account: { username: "", password: "" },
     errors: {}
   };
 
-  schema = {
-    username: Joi.string()
-      .required()
-      .label("Username"),
-    password: Joi.string()
-      .required()
-      .label("Password")
+  validate = () => {
+    const errors = {};
+
+    const { account } = this.state;
+    if (account.username.trim() === "")
+      errors.username = "Username is required";
+    if (account.password.trim() === "")
+      errors.password = "Password is required";
+    return Object.keys(errors).length === 0 ? null : errors;
   };
 
-  doSubmit = () => {
-    // call serveer
+  handleLogin = e => {
+    e.preventDefault();
+
+    const errors = this.validate();
+
+    this.setState({ errors: errors || {} });
+    if (errors) return;
+
     console.log("submitted");
   };
 
+  handleChange = ({ currentTarget: input }) => {
+    const account = { ...this.state.account };
+    account[input.name] = input.value;
+
+    this.setState({ account });
+  };
   render() {
     const { account, errors } = this.state;
     return (
       <div>
         <h1>Login</h1>
-        <form onSubmit={this.handleSubmit}>
+        <form onSubmit={this.handleLogin}>
           <Input
             name="username"
             value={account.username}
@@ -54,11 +66,7 @@ class Login extends Form {
               </label>
             </div>
           </div>
-          <button
-            disabled={this.validate()}
-            type="submit"
-            className="btn btn-primary"
-          >
+          <button type="submit" className="btn btn-primary">
             Login
           </button>
         </form>
